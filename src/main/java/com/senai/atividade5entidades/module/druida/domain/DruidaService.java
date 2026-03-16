332e943e8aae1c90c82ca86d5854689d8769745a
heloo86 <heloisa.af86@gmail.com>
2026-03-16 20:48:46 -0300

    chore: add druida's domain

--- src/main/java/com/senai/atividade5entidades/module/druida/domain/DruidaService.java
+++ src/main/java/com/senai/atividade5entidades/module/druida/domain/DruidaService.java
@@ -1,4 +1,46 @@
 package com.senai.atividade5entidades.module.druida.domain;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class DruidaService {
+    private final DruidaRepository repository;
+
+    public Druida save (Druida Druida){
+        return repository.save(Druida);
+    }
+
+    public List<Druida> list (){
+        return repository.findAll();
+    }
+
+    public Druida findById (Long id){
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Druida não encontrado"));
+    }
+
+    public Druida update (Long id, Druida druida){
+        Druida druidaSalvo = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Druida não encontrado"));
+
+        druidaSalvo.setNome(druida.getNome());
+        druidaSalvo.setNivel(druida.getNivel());
+        druidaSalvo.setSabedoria(druida.getSabedoria());
+        druidaSalvo.setCargasFormaSelvagem(druida.getCargasFormaSelvagem());
+        druidaSalvo.setCirculoDruidico(druida.getCirculoDruidico());
+
+        return repository.save(druidaSalvo);
+    }
+
+    public List<Druida> findAll(){
+        return repository.findAll();
+    }
+
+    public void deleteById (Long id){
+        repository.deleteById(id);
+    }
 }
